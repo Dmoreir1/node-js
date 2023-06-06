7dc56082fb6310a6ac0ed31dbbbc5d2ece7ad17c
dmoreir1 <dmoreir1@binghamton.edu>
2023-06-06 16:40:41 -0400

    Exercise 1

---
+++ index.js
@@ -0,0 +1,32 @@
+const cors = require('cors')
+const express = require('express')
+const { body, check, param, validationResult } = require('express-validator')
+
+const PORT = 8080
+const app = express()
+const corsOptions = { origin: ['http://localhost:3000'], optionsSuccessStatus: 200 }
+
+// Middleware...
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Your endpoints here..
+
+
+app.get('/person/:id', cors(corsOptions), async (req, res) => { 
+    // const result = await mySqlProxy.<YOUR FUNCTION HERE>
+    // const id = req.params['id']                  // Read parameters from URL.
+    // const personType = req.query['personType']   // Read query parameters from URL.
+    // const body = req.body                        // Read request body.
+    // res.send(<YOUR OBJECT HERE>)
+    res.send({message: 'Hello World'})
+})
+
+app.get('/message', cors(corsOptions), async(req, res) => {
+    res.send({message: 'Hi my name is Ariana Grande !'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Express web API running on port: ${PORT}.`)
+})
